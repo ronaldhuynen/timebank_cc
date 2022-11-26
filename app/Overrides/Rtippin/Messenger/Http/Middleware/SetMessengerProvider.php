a93276551e49d770fe6e47b9e318a8f2e24efdd2
ronaldhuynen <ronald.huynen@timebanmk.cc>
2022-11-26 23:15:17 +0100

    Set Messenger Provider vendor override method

---
+++ app/Overrides/Rtippin/Messenger/Http/Middleware/SetMessengerProvider.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace RTippin\Messenger\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use RTippin\Messenger\Messenger;
+use Illuminate\Support\Facades\Auth;
+use RTippin\Messenger\Exceptions\InvalidProviderException;
+
+class SetMessengerProvider
+{
+    /**
+     * @var Messenger
+     */
+    protected Messenger $messenger;
+
+    /**
+     * SetMessengerProvider constructor.
+     *
+     * @param  Messenger  $service
+     */
+    public function __construct(Messenger $service)
+    {
+        $this->messenger = $service;
+    }
+
+    /**
+     * Attempt to set the current MessengerProvider for
+     * this request cycle as the authenticated user.
+     *
+     * @param  Request  $request
+     * @param  Closure  $next
+     * @param  null  $required
+     * @return mixed
+     *
+     * @throws InvalidProviderException
+     */
+    public function handle(Request $request, Closure $next, $required = null)
+    {
+        $this->setProvider($request);
+
+        if ($required === 'required' && ! $this->messenger->isProviderSet()) {
+            $this->messenger->throwProviderError();
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Set the provider! You may override this method if you plan
+     * to set your provider using different authentication
+     * methods or guards.
+     *
+     * @param  Request  $request
+     *
+     * @throws InvalidProviderException
+     */
+    protected function setProvider(Request $request): void
+    {
+        // Override method
+        if ($request->user()) {
+
+            if (Session('activeProfileType')) {
+            $class = new (Session('activeProfileType'));
+            $provider = $class->find(Session('activeProfileId'));
+            } else {
+                $provider = $request->user();
+            }
+            $this->messenger->setProvider(
+                $provider
+            );
+        }
+    }
+}
+

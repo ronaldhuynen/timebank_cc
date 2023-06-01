13ac1546a1fda35b841a4bc26b2570de117ce68c
ronaldhuynen <ronald.huynen@timebanmk.cc>
2023-06-01 08:58:43 +0200

    Previous login on Dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -4,21 +4,32 @@ namespace App\Http\Livewire;
 
 use App\Models\User;
 use Illuminate\Support\Carbon;
-use Spatie\Activitylog\Models\Activity;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use Spatie\Activitylog\Models\Activity;
 
 class Dashboard extends Component
 {
+    public $lastLoginAt;
+    public $user;
 
     public function mount()
     {
+        $this->user = [
+            'name' => auth()->user()->name,
+            'firstName' => Str::words(auth()->user()->name, 1,''),
+            'birthday' => auth()->user()->date_of_birth,
+        ];
+
         $activityLog =
         Activity::where('subject_id', auth()->user()->id)
-        ->where('subject_type','App\Models\User')
-        ->whereNotNull('properties->attributes->last_login_at')
+        ->where('subject_type', 'App\Models\User')
+        ->whereNotNull('properties->old->last_login_at')
         ->get('properties')->last();
-        $lastLoginAt = json_decode($activityLog, true)['properties']['old']['last_login_at'];
-        $this->lastLoginAt = Carbon::createFromTimeStamp(strtotime($lastLoginAt))->diffForHumans();
+        if(isset($activityLog)) {
+            $lastLoginAt = json_decode($activityLog, true)['properties']['old']['last_login_at'];
+            $this->lastLoginAt = Carbon::createFromTimeStamp(strtotime($lastLoginAt))->diffForHumans();
+        }
     }
 
 
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,6 +1,7 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    @if($lastLoginAt)
      <div class="float-right text-xs text-gray-500">
-        {{ __('Last login') . ' ' . $lastLoginAt }}
+        {{ __('Previous login') . ': ' . $lastLoginAt }}
     </div>
 
     <div>
@@ -8,7 +9,7 @@
     </div>
 
     <div class="mt-8 text-2xl">
-        Welcome to your dashboard!
+        Time changes everything!
     </div>
 
     <div class="mt-6 text-gray-500">
@@ -17,6 +18,27 @@
         you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
         ecosystem to be a breath of fresh air. We hope you love it.
     </div>
+
+    @else
+     <div class="float-right text-xs text-gray-500">
+        {{ __('First login session') }}
+    </div>
+    <div>
+        <x-jet-application-logo class="block h-12 w-auto" />
+    </div>
+
+    <div class="mt-8 text-2xl">
+        Welcome {{ $user['firstName'] }}, this is your dashboard !
+    </div>
+
+    <div class="mt-6 text-gray-500">
+        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
+        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
+        ecosystem to be a breath of fresh air. We hope you love it.
+    </div>
+    @endif
+
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">

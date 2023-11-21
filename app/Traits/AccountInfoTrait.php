1f626bd7629a69c0b300a5735eb0face65d0ed06
ronaldhuynen <ronald.huynen@timebanmk.cc>
2023-11-22 00:06:48 +0100

    getAccountTotals: sumBalances, countTransfers etc

--- app/Traits/AccountInfoTrait.php
+++ app/Traits/AccountInfoTrait.php
@@ -6,15 +6,6 @@ use App\Models\Transaction;
 
 trait AccountInfoTrait
 {
-    /**
-     * AccountInfoTrait constructor.
-     * This constructor initializes the middleware for authentication.
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
 
     public function getBalance($accountId)
     {
@@ -41,6 +32,36 @@ trait AccountInfoTrait
      */
     public function getAccountsInfo($profileType = null, $profileId = null)
     {
+    if ($profileType === null) {
+        $profileType = session('activeProfileType');
+    }
+    if ($profileId === null) {
+        $profileId = session('activeProfileId');
+    }
+
+    $accounts = $profileType::find($profileId)->accounts;
+    $accounts = $accounts->map(function ($account) {
+        return [
+            'id' => $account->id,
+            'name' => $account->name,
+            'balance' => $this->getBalance($account->id)
+            ];
+        });
+        return $accounts;
+
+    }
+    
+
+    /**
+     * Retrieves the account totals of a profile.
+     *
+     * @param string|null $profileType The profile type. If null, the active profile type from the session will be used.
+     * @param int|null $profileId The profile ID. If null, the active profile ID from the session will be used.
+     * @param int|null $sinceDaysAgo The number of days to filter transactions. If null, all transactions will be included.
+     * @return array Sum of all balances, count of transfers, count of transfers received, count of transfers sent.
+     */
+    public function getAccountsTotals($profileType = null, $profileId = null, $sinceDaysAgo = null)
+    {
         if ($profileType === null) {
             $profileType = session('activeProfileType');
         }
@@ -49,13 +70,48 @@ trait AccountInfoTrait
         }
 
         $accounts = $profileType::find($profileId)->accounts;
-        $accounts = $accounts->map(function ($account) {
-            return [
-                'id' => $account->id,
-                'name' => $account->name,
-                'balance' => $this->getBalance($account->id)
-            ];
-        });
-        return $accounts;
+        
+        $sumBalances = 0; 
+        $countTransfers = 0; 
+        $countTransfersReceived = 0; 
+        $countTransfersSent = 0;
+
+        $accountIds = $accounts->pluck('id')->toArray(); // Get all account ids of profile
+
+        foreach ($accounts as $account) {
+            $sumBalances  += $this->getBalance($account->id);
+            $transfersQuery = Transaction::where('from_account_id', $account->id)->orWhere('to_account_id', $account->id);
+            
+            // If $sinceDaysAgo is not null, filter transactions created within the specified number of days
+            if ($sinceDaysAgo !== null) {
+                $transfersQuery->whereDate('created_at', '>=', now()->subDays($sinceDaysAgo));
+            }
+
+            $transfers = $transfersQuery->get();
+
+            foreach ($transfers as $transfer) {
+                // Exclude transactions between accounts owned by the same profile
+                // Check if both from_account_id and to_account_id are in $accountIds
+                if (!in_array($transfer->from_account_id, $accountIds) || !in_array($transfer->to_account_id, $accountIds)) {
+                    $countTransfers++;
+                    if ($transfer->to_account_id == $account->id) {
+                        $countTransfersReceived++;
+                    }
+                    if ($transfer->from_account_id == $account->id) {
+                        $countTransfersSent++;
+                    }
+                }
+            }
+        }
+        
+        $totals = [
+            'sumBalances' => $sumBalances,
+            'countTransfersSince' => now()->subDays($sinceDaysAgo),
+            'transfers' => $countTransfers,
+            'transfersReceived' => $countTransfersReceived,
+            'transfersSent' => $countTransfersSent,
+        ];
+
+        return $totals;   
     }
 }

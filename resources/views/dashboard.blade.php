c8c1265bf9617b61c56d553baa8779c8f0728182
ronaldhuynen <ronald.huynen@timebanmk.cc>
2024-06-03 13:18:10 +0200

    Upgrade spartie/medialibrary

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,10 +17,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div>
-    </div>
-
-      {{exec('whoami')}};
-      
+    </div>      
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <livewire:dashboard>
             </div>

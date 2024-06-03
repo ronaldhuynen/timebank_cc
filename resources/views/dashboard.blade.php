35f52eb2700bd160cec1ad6b80bf2edfaad19dc8
ronaldhuynen <ronald.huynen@timebanmk.cc>
2024-06-03 12:44:45 +0200

    who am i debug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,6 +18,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div>
     </div>
+
+      {{exec('whoami')}};
+      
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <livewire:dashboard>
             </div>

429cb6327847e9884f60748811c4a30a1ce4ad5b
ronaldhuynen <ronald.huynen@timebanmk.cc>
2024-06-17 01:44:18 +0200

    Fix pusher connection Client side

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -13,9 +13,9 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_PUSHER_HOST,
-    wsPort: process.env.MIX_PUSHER_PORT,
-    wssPort: process.env.MIX_PUSHER_PORT,
+    // wsHost: process.env.MIX_PUSHER_HOST,
+    // wsPort: process.env.MIX_PUSHER_PORT,
+    // wssPort: process.env.MIX_PUSHER_PORT,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: false,
     encrypted: false,

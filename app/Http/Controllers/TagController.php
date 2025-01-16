1d651ea6e96d51da4b645e7f079d6336bccffb71
Ronald Huynen <ronald.huynen@timebanmk.cc>
2025-01-16 10:31:42 +0100

    Secure TagController

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class TagController extends Controller
 {
     public function manage()
-    {
+    {    
+        if (getActiveProfileType() !== 'Admin') {
+            abort(403, __('Admin profile required'));
+        }
+
         return view('tags.manage');
     }
 }

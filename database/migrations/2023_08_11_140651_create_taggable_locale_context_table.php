c900bd35676f81fc1dfa365f424d11ec893ee02e
Ronald Huynen <ronald.huynen@timebanmk.cc>
2025-03-17 13:34:36 +0100

    Create_taggable_locale_context: Make tag_id unique

--- database/migrations/2023_08_11_140651_create_taggable_locale_context_table.php
+++ database/migrations/2023_08_11_140651_create_taggable_locale_context_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('taggable_locale_context', function (Blueprint $table) {
-            $table->id();            
-            $table->unsignedBigInteger('tag_id'); // Foreign key column
+            $table->id();
+            $table->unsignedBigInteger('tag_id')->unique(); // Foreign key column
             $table->unsignedBigInteger('context_id'); // Foreign key column
 
             $table->foreign('tag_id')->references('tag_id')->on('taggable_tags')->onDelete('cascade');     // Many-to-many relation
